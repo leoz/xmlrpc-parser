81810865e0c599e9a2db86e7e5c8580c60ac4986
Leonid Zolotarev <lzolotarev@magicleap.com>
2020-03-10 18:25:43 -0400

    Add jest tests

---
+++ __tests__/fetchXml.spec.js
@@ -0,0 +1,21 @@
+//
+import fetch from 'cross-fetch';
+import { XmlRpcMessage, XmlRpcResponse } from '../src/index';
+
+describe('Fetch and parse XML funtion', () => {
+  test('It should fetch and parse XML', async () => {
+    const method = 'examples.getStateName';
+    const params = 23;
+    const message = new XmlRpcMessage(method, [params]);
+    const input = message.xml();
+    const URL = 'http://betty.userland.com/RPC2';
+    //
+    const res = await fetch(URL, { method: 'post', body: input });
+    const xml = await res.text();
+    //
+    const response = new XmlRpcResponse();
+    const result = await response.parse(xml);
+    //
+    expect('Minnesota').toBe(result.params[0]);
+  });
+});
---
+++ __tests__/messageToXml.spec.js
@@ -0,0 +1,37 @@
+//
+import { XmlRpcMessage } from '../src/index';
+
+describe('Fetch XML funtion', () => {
+  test('it should fetch XML', () => {
+    const method = 'MyCall';
+    const params = {
+      a: 1,
+      b: 2
+    };
+    const message = new XmlRpcMessage(method, [params]);
+    const input = message.xml();
+
+    const output =
+      '<?xml version="1.0"?>\n' +
+      '<methodCall>\n' +
+      '<methodName>MyCall</methodName>\n' +
+      '<params>\n' +
+      '<param>\n' +
+      '<value><struct>\n' +
+      '<member>\n' +
+      '<name>a</name>\n' +
+      '<value><i4>1</i4></value>\n' +
+      '</member>\n' +
+      '<member>\n' +
+      '<name>b</name>\n' +
+      '<value><i4>2</i4></value>\n' +
+      '</member>\n' +
+      '</struct>\n' +
+      '</value>\n' +
+      '</param>\n' +
+      '</params>\n' +
+      '</methodCall>';
+
+    expect(input).toEqual(output);
+  });
+});
